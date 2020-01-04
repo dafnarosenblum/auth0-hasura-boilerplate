d544d1cc7fe97c321631e3523445cfdaa0617ed7
dafnarosenblum <dafi.moin@gmail.com>
2020-01-04 14:00:15 +0100

    Use new contex for real user fetched from Auth0

--- components/Main.js
+++ components/Main.js
@@ -1,18 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useAuth } from "use-auth0-hooks";
 
 export default function Main() {
-  const x = useContext(AuthContext);
+  const { authUser, setAuthUser } = useContext(AuthContext);
+  const { user, isLoading } = useAuth();
+
+  useEffect(() => {
+    if (user && !authUser) {
+      setAuthUser(user.given_name);
+    }
+  });
 
   return (
     <div style={{ marginTop: "5rem" }}>
-      <button
-        style={{ margin: "0 1rem" }}
-        onClick={() => x.setAuthUser("test context update succeeded")}
-      >
-        test
-      </button>
-      authUser.user.permissions["x-hasura-user-id"]: {x.authUser}
+      {isLoading
+        ? "Loading..."
+        : `authUser.user.permissions["x-hasura-user-id"]: ${authUser}`}
       {/* {authUser.user.permissions["x-hasura-user-id"]} */}
     </div>
   );
